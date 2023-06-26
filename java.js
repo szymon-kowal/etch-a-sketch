5f66139d2bdeabcde29b432e6fcb83b09503aa62
szymon-kowal <00szymon.kowal@gmail.com>
2023-06-26 17:58:48 +0200

    Add switching color when buttons are clicked.

--- java.js
+++ java.js
@@ -1,14 +1,41 @@
-const button16 = document.getElementsByClassName('16')[0];
 const container = document.getElementsByClassName('container')[0];
-const rainbow_butt = document.getElementsByClassName('RGB')[0];
+const rainbow_button = document.getElementsByClassName('button-85')[0];
 const clear = document.getElementsByClassName('clear')[0];
 const inputField = document.getElementsByClassName('inputField')[0];
 const submitButton = document.getElementsByClassName('submitButton')[0];
+const black_button = document.getElementsByClassName('button-black')[0];
 let childElements;
+let colorValue = 0;
+const style = document.createElement('style');
+style.innerHTML = '.button-85:after { background: #E8A2DC; }';
+
+const style2 = document.createElement('style');
+style2.innerHTML =  '.button-black:after { background: #E8A2DC; }';
+
+
 
 // Input box / check if inputs is ok. // Finds all child elements
 let inputValue = 16;
 addSquare(inputValue);
+
+// Switch background colour if black and rgb buttons are clicked 
+// and switch the colors during painting on container.
+rainbow_button.addEventListener('click', () => {
+    // Append the style rule to the document's head
+    document.head.appendChild(style);
+    document.head.removeChild(style2);
+    colorValue = 1;
+});
+
+
+
+black_button.addEventListener('click', () => {
+    // Append the style rule to the document's head
+    document.head.appendChild(style2);
+    document.head.removeChild(style);
+});
+
+
 
 submitButton.addEventListener('click', () => {
     inputValue = parseInt(inputField.value);
@@ -34,17 +61,16 @@ let isMousePressed = false;
 document.addEventListener('mousedown',function(event) {
     if (event.button === 0) {
         isMousePressed = true;
-        console.log('mosue down')
     }
 })
 
 document.addEventListener('mouseup',function(event) {
     if (event.button === 0) {
         isMousePressed = false;
-        console.log('mosue up')
     }
 })
-// Renove squares from parent node
+
+// Remove squares from parent node
 function removeChildNode(parent) {
     while (parent.firstChild) {
         parent.removeChild(parent.firstChild);
@@ -66,7 +92,6 @@ function addSquare(input) {
         square.style.backgroundColor = "white";
         // Add eventListener to every div
         square.addEventListener('mouseenter', () => {
-            console.log('mouse enter')
             if (isMousePressed == true) {
                 square.style.backgroundColor = 'black';
             }
